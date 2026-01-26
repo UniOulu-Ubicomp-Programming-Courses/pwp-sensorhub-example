1fa2c25cab6da8cdaa7cc99ff1b573e26be5f7c7
Mika Oja <enk@kapsi.fi>
2026-01-26 11:56:48 +0200

    initial

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask
+import json
+from datetime import datetime
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.engine import Engine
@@ -9,19 +11,20 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
-deployments = db.Table("deployments",
-    db.Column("deployment_id", db.Integer, db.ForeignKey("deployment.id"), primary_key=True),
-    db.Column("sensor_id", db.Integer, db.ForeignKey("sensor.id"), primary_key=True)
-)
-
 @event.listens_for(Engine, "connect")
 def set_sqlite_pragma(dbapi_connection, connection_record):
     cursor = dbapi_connection.cursor()
     cursor.execute("PRAGMA foreign_keys=ON")
     cursor.close()
 
+deployments = db.Table("deployments",
+    db.Column("deployment_id", db.Integer, db.ForeignKey("deployment.id"), primary_key=True),
+    db.Column("sensor_id", db.Integer, db.ForeignKey("sensor.id"), primary_key=True)
+)
+
 class Location(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), nullable=False)
     latitude = db.Column(db.Float, nullable=True)
     longitude = db.Column(db.Float, nullable=True)
     altitude = db.Column(db.Float, nullable=True)
@@ -31,8 +34,8 @@ class Location(db.Model):
 
 class Deployment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    start = db.Column(db.DateTime, nullable=True)
-    end = db.Column(db.DateTime, nullable=True)
+    start = db.Column(db.DateTime, nullable=False)
+    end = db.Column(db.DateTime, nullable=False)
     name = db.Column(db.String(128), nullable=False)
     
     sensors = db.relationship("Sensor", secondary=deployments, back_populates="deployments")
@@ -55,3 +58,30 @@ class Measurement(db.Model):
         
     sensor = db.relationship("Sensor", back_populates="measurements")
 
+@app.route("/<sensor_name>/measurements/add/", methods=["POST"])
+def add_measurement(sensor_name):
+    # This branch happens when user submits the form
+    try:
+        sensor = Sensor.query.filter_by(name=sensor_name).first()
+        if sensor:
+            value = float(request.json["value"])
+            meas = Measurement(
+                sensor=sensor,
+                value=value,
+                time=datetime.now()
+            )
+            db.session.add(meas)
+            db.session.commit()
+            return "", 201
+        else:
+            abort(404)
+    except (KeyError, ValueError, IntegrityError):
+        abort(400)
+
+@app.route("/sensors/")
+def get_sensors():
+    response_data = []
+    sensors = Sensor.query.all()
+    for sensor in sensors:
+        response_data.append([sensor.name, sensor.model])        
+    return json.dumps(response_data)
