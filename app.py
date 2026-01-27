0902f3e9e34de180ab868c2b56ddf703469d4410
Mika Oja <enk@kapsi.fi>
2026-01-27 09:47:45 +0200

    Added missing imports

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import json
 from datetime import datetime
-from flask import Flask, request
+from flask import Flask, Response, request
 from flask_restful import Api, Resource
 from flask_sqlalchemy import SQLAlchemy
+from jsonschema import ValidationError, validate
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.engine import Engine
 from sqlalchemy import event
-from werkzeug.exceptions import NotFound
+from werkzeug.exceptions import BadRequest, Conflict, NotFound, UnsupportedMediaType
 from werkzeug.routing import BaseConverter
 
 app = Flask(__name__)
@@ -143,21 +144,26 @@ class SensorCollection(Resource):
 
     def post(self):
         if not request.json:
-            abort(415)
+            raise UnsupportedMediaType
 
         try:
-            sensor = Sensor(
-                name=request.json["name"],
-                model=request.json["model"],
-            )
+            validate(request.json, Sensor.json_schema())
+        except ValidationError as e:
+            raise BadRequest(description=str(e))
+
+        sensor = Sensor()
+        sensor.deserialize(request.json)
+        try:
             db.session.add(sensor)
             db.session.commit()
-        except KeyError:
-            abort(400)
         except IntegrityError:
-            abort(409)
+            raise Conflict(
+                description="Sensor with name '{name}' already exists.".format(
+                    **request.json
+                )
+            )
 
-        return "", 201
+        return Response(status=201)
 
 class SensorItem(Resource):
 
@@ -179,7 +185,6 @@ class SensorItem(Resource):
             db.session.commit()
         except IntegrityError:
             raise Conflict(
-                409,
                 description="Sensor with name '{name}' already exists.".format(
                     **request.json
                 )
