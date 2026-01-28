418f1704232cd3f0fe7291ef4a0c77130ae2d995
Mika Oja <enk@kapsi.fi>
2026-01-28 11:18:17 +0200

    Removed tempfile use because it doesn't work with current versions of flask-sqlalchemy

--- db_test.py
+++ db_test.py
@@ -20,10 +20,6 @@ def set_sqlite_pragma(dbapi_connection, connection_record):
 # we don't need a client for database testing, just the db handle
 @pytest.fixture
 def db_handle():
-    db_fd, db_fname = tempfile.mkstemp()
-    app.app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + db_fname
-    app.app.config["TESTING"] = True
-    
     ctx = app.app.app_context()
     ctx.push()
     app.db.create_all()
@@ -34,8 +30,6 @@ def db_handle():
     app.db.drop_all()
     app.db.session.remove()
     ctx.pop()
-    os.close(db_fd)
-    os.unlink(db_fname)
 
 def _get_location(sitename="alpha"):
     return Location(
